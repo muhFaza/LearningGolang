8eccaa97f5641dd526c68e26bde227e042a905cc
muhFaza <fazamuhammad30@gmail.com>
2023-12-11 16:02:02 +0700

    daily challenge 2 done

--- 01hello/main.go
+++ 01hello/main.go
@@ -1,7 +1,6 @@
 package main
 import (
 	"fmt"
-	"another/printme"
 )
 func main() {
 	// var name string = "faza"
---
+++ 02condition/dailychallenge2.go
@@ -0,0 +1,48 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("DAILY CHALLENGE 2")
+
+	// NUMBER 1
+	fmt.Println("NUMBER 1")
+	type person = map[string]string
+
+	var people = []map[string]string{
+		{
+			"name": "Hank",
+			"Age": "50",
+			"Job": "Polisi",
+		},
+		{
+			"name": "Heisenberg",
+		"Age": "52",
+		"Job": "Ilmuwan",
+	},
+		{"name": "Skyler",
+		"Age": "48",
+		"Job": "Akuntan",},
+	}
+
+	for _, v := range people {
+		fmt.Printf("Hi Perkenalkan, Nama saya %v, umur saya %v, dan saya bekerja sebagai %v \n", v["name"], v["Age"], v["Job"])
+	}
+
+	// NUMBER 2
+	fmt.Println("NUMBER 2")
+	var rows1 int = 5
+	for rows1 > 0 {
+		fmt.Println("*")
+		rows1--
+	}
+	
+	// NUMBER 3
+	fmt.Println("NUMBER 3")
+	var rows3 int = 5
+	for i := 0; i < rows3; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Printf("*")
+		}
+		fmt.Printf("\n")
+	}
+}
