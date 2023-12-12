c4fc2b5375f07532006a699c840cecc9911ea6c4
muhFaza <fazamuhammad30@gmail.com>
2023-12-12 12:19:10 +0700

    refactoring fibonacci

--- 03function/dailychallenge3.go
+++ 03function/dailychallenge3.go
@@ -16,7 +16,6 @@ func main() {
 	fmt.Println("\nCHALLENGE 2")
 	fibo := Fibonacci(n)
 	fmt.Println("Bilangan Fibonacci ke", n, "=", fibo)
-
 }
 
 func AlayGen(input ...string) string {
@@ -48,21 +47,20 @@ func Fibonacci(n int) int {
 	a, b, c := 0, 1, 0
 	n++
 	for i := 0; i < n; i++ {
-		if i > 2 {
-			// Ganjil replace a
-			if i%2 == 1 {
-				a = c
-			}
-			// Genap replace b
-			if i%2 == 0 {
-				b = c
-			}
-		}
-		if i == 0 || i == 1 {
+
+		if i < 2 {
 			c = i
-		} else {
-			c = a + b
+			continue
 		}
+		c = a + b
+		
+		// Genap replace a dgn hasil c
+		// Ganjil replace b dgn hasil c
+		if i%2 == 0 {
+			a = c
+			continue
+		}
+		b = c
 	}
 	return c
 }
